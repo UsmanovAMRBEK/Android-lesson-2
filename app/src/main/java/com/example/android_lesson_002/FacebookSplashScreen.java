4e6f60d2ed4f5605f9fe348c752df3458757d364
UsmanovAMRBEK <princariens@gmail.com>
2023-07-28 14:43:41 +0500

    one :)

---
+++ app/src/main/java/com/example/android_lesson_002/FacebookSplashScreen.java
@@ -0,0 +1,14 @@
+package com.example.android_lesson_002;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FacebookSplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_splash_screen);
+    }
+}
---
+++ app/src/main/java/com/example/android_lesson_002/TwitterSplashScreen.java
@@ -0,0 +1,14 @@
+package com.example.android_lesson_002;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwitterSplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter_splash_screen);
+    }
+}
